82bcd354631e64dccaaf4b694c7124b08d26036c
captainbn <bnwyyx217@163.com>
2019-04-14 19:19:49 +0800

    init

---
+++ src/main/java/com/xishanqu/videoshop/VideoshopApplication.java
@@ -0,0 +1,13 @@
+package com.xishanqu.videoshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoshopApplication.class, args);
+    }
+
+}
